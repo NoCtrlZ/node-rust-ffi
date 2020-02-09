0a9c6c6ecde8911f7f80764f9eb8c3d265ac465b
Hemlocked <phantomofrotten@gmail.com>
2020-02-09 16:42:35 +0900

    steg success

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,10 +1,19 @@
 #[macro_use]
 extern crate neon;
+extern crate steganography;
 
 use neon::prelude::*;
+use steganography::encoder::*;
+use steganography::util::*;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+fn steg(mut cx: FunctionContext,) -> JsResult<JsString> {
+    let message = "This is a steganography demo!".to_string();
+    let payload = str_to_bytes(&message);
+    let destination_image = file_as_dynamic_image("example.png".to_string());
+    let enc = Encoder::new(payload, destination_image);
+    let result = enc.encode_alpha();
+    save_image_buffer(result, "hidden_message.png".to_string());
+    Ok(cx.string("steg"))
 }
 
-register_module!(mut cx, { cx.export_function("hello", hello) });
+register_module!(mut cx, { cx.export_function("steg", steg) });
