7e1361f3618187376a75eb338c4b05b8f3e833c3
Hemlocked <phantomofrotten@gmail.com>
2020-02-09 17:41:14 +0900

    encode and decode

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,9 +4,10 @@ extern crate steganography;
 
 use neon::prelude::*;
 use steganography::encoder::*;
+use steganography::decoder::*;
 use steganography::util::*;
 
-fn steg(mut cx: FunctionContext) -> JsResult<JsString> {
+fn encode(mut cx: FunctionContext) -> JsResult<JsString> {
     let message = cx.argument::<JsString>(0)?.value();
     let input = cx.argument::<JsString>(1)?.value();
     let output = cx.argument::<JsString>(2)?.value();
@@ -15,14 +16,25 @@ fn steg(mut cx: FunctionContext) -> JsResult<JsString> {
     let enc = Encoder::new(payload, destination_image);
     let result = enc.encode_alpha();
     save_image_buffer(result, output);
-    Ok(cx.string("done"))
+    Ok(cx.string("encode"))
 }
 
-register_module!(mut cx, { cx.export_function("steg", steg) });
+fn decode(mut cx: FunctionContext) -> JsResult<JsString> {
+    let input = cx.argument::<JsString>(0)?.value();
+    let encoded_image = file_as_image_buffer(input);
+    let dec = Decoder::new(encoded_image);
+    let out_buffer = dec.decode_alpha();
+    let clean_buffer: Vec<u8> = out_buffer.into_iter()
+                                        .filter(|b| {
+                                            *b != 0xff_u8
+                                        })
+                                        .collect();
+    let message = bytes_to_str(clean_buffer.as_slice());
+    Ok(cx.string(message.to_string()))
+}
 
-// fn args(mut cx: FunctionContext) -> JsResult<JsString> {
-//     let arg0 = cx.argument::<JsString>(0)?.value();
-//     Ok(cx.string(arg0))
-// }
-
-// register_module!(mut cx, { cx.export_function("args", args) });
+register_module!(mut cx, {
+    cx.export_function("encode", encode)?;
+    cx.export_function("decode", decode)?;
+    Ok(())
+});
