a41a837fac8233997272fc3ccbfc08cd9f730847
Hemlocked <phantomofrotten@gmail.com>
2020-02-09 16:24:45 +0900

    initialize project

---
+++ native/src/lib.rs
@@ -0,0 +1,10 @@
+#[macro_use]
+extern crate neon;
+
+use neon::prelude::*;
+
+fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(cx.string("hello node"))
+}
+
+register_module!(mut cx, { cx.export_function("hello", hello) });
